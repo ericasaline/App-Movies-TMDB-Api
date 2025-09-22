075a364d21ff3c590a4dc536717683da6faf9056
ericasaline <ericasaline@gmail.com>
2025-09-22 14:52:39 -0300

    ajuste chamada api

--- app/app/src/main/java/br/com/app/movie/data/remote/service/MovieService.kt
+++ app/app/src/main/java/br/com/app/movie/data/remote/service/MovieService.kt
@@ -7,23 +7,23 @@ import br.com.app.movie.data.remote.dto.MoviesDto
 import retrofit2.http.GET
 import retrofit2.http.Path
 
-const val api_key = "" //TODO: mover
+const val api_key = ""
 
 interface MovieService {
 
-    @GET("movie/now_playing?$api_key")
+    @GET("movie/now_playing?$api_key&language=pt-BR")
     suspend fun getNowPlaying(): MoviesDto
 
-    @GET("movie/upcoming?$api_key")
+    @GET("movie/upcoming?$api_key&language=pt-BR")
     suspend fun getUpcoming(): MoviesDto
 
-    @GET("movie/{movie_id}?$api_key")
+    @GET("movie/{movie_id}?$api_key&language=pt-BR")
     suspend fun getDetails(@Path("movie_id") id: Int): DetailsDto
 
-    @GET("movie/{movie_id}/images?$api_key")
+    @GET("movie/{movie_id}/images?$api_key&language=pt-BR")
     suspend fun getImages(@Path("movie_id") id: Int): ImagesDto
 
-    @GET("movie/{movie_id}/credits?$api_key")
+    @GET("movie/{movie_id}/credits?$api_key&language=pt-BR")
     suspend fun getCredits(@Path("movie_id") id: Int): CreditsDto
 
 }
--- app/app/src/main/java/br/com/app/movie/ui/viewmodel/MovieViewModel.kt
+++ app/app/src/main/java/br/com/app/movie/ui/viewmodel/MovieViewModel.kt
@@ -3,20 +3,12 @@ package br.com.app.movie.ui.viewmodel
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import br.com.app.movie.data.remote.api.ApiResult
-import br.com.app.movie.data.remote.dto.MoviesDto
 import br.com.app.movie.data.repository.MovieRepository
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
 class MovieViewModel(
     private val repository: MovieRepository
 ): ViewModel() {
-
-
-    private val _movies = MutableStateFlow<ApiResult<MoviesDto>>(ApiResult.Loading)
-    val movies = _movies
-
 
     fun teste() {
         viewModelScope.launch {
@@ -41,6 +33,4 @@ class MovieViewModel(
             }
         }
     }
-
-
 }
